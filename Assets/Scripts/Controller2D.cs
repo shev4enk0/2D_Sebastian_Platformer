8e5f37c59d6b6a010a2312ec9832f29e0a74d67e
shev4enk0 <33554718+shev4enk0@users.noreply.github.com>
2017-12-15 11:54:18 +0200

    Begin

---
+++ Assets/Scripts/Controller2D.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Player))]
+public class Controller2D : MonoBehaviour
+{
+    struct RaycastOrigins
+    {
+        public  Vector2 topLeft, topRight, bottomLeft, bottomRight;
+    }
+
+    BoxCollider2D boxCollider;
+
+    // Use this for initialization
+    void Start()
+    {
+        boxCollider = GetComponent < BoxCollider2D>();
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+
+}
